b0d81b728de1a64a0ca13b99e940311c2d09fe9d
haneru <haneru3@gmail.com>
2018-11-03 11:44:28 +0900

    Generate: Fileds Controller

---
+++ test/controllers/fields_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class FieldsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get fields_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get fields_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get fields_edit_url
+    assert_response :success
+  end
+
+end
